eb5d2e2a7f2910ecde5b9709447498a5cb829f8d
악동분홍토끼 <pinkrabbit412@daum.net>
2019-10-05 22:05:08 +0900

    v1.0.1.rel191005A
    1. Make readability better.
    2. Design & Script modified.
    3. Changed favicon, etc.

--- scripts/pageSizeAdjust.js
+++ scripts/pageSizeAdjust.js
@@ -2,11 +2,27 @@ $(document).ready(
     function primaryAdjust() {
         var pageWidth = $( window ).width();
         if(pageWidth < 425) {
-            $('html').css('font-size', '45%');
-            $('.headerText').css('display', 'none');
+            //이 곳이 호출되면 모바일 환경에서 접속한 것으로 간주함
+            
+            //header.html
+            $('html').css('font-size', '75%');
+            $('.headerIcon').attr('height', '18px');
+            $('.headerRavitLogo').attr('height', '33px');
+            $('.headerText').css('display', 'none'); //아니 이거 왜 적용안돼에에ㅔㅔㅔ
+
+            //faq.html
+            $('.ques').css('padding', '10px');
+            $('.ques').css('padding-left', '17px');
+            $('.ques').css('padding-right', '20px');
+            $('.ans').css('padding', '10px');
+            $('.ans').css('padding-left', '17px');
+            $('.ans').css('padding-right', '20px');
+
+            //campusMap.html
+            $('#mapImageSelector button').css('width', '47%');
         }
         else if(pageWidth < 575) {
-            $('html').css('font-size', '55%');
+            $('html').css('font-size', '70%');
             $('.headerText').css('display', 'none');
         }
         else if (pageWidth < 800) {
@@ -25,11 +41,11 @@ $(window).resize(
     function autoAdjust() {
         pageWidth = $(window).width();
         if(pageWidth < 425) {
-            $('html').css('font-size', '45%');
+            //$('html').css('font-size', '45%');
             $('.headerText').css('display', 'none');
         }
         else if(pageWidth < 575) {
-            $('html').css('font-size', '55%');
+            //$('html').css('font-size', '55%');
             $('.headerText').css('display', 'none');
         }
         else if (pageWidth < 800) {
